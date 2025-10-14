137b395e3911e58f9380752bf98ec8414814cc7d
Xumoyuddin <xumoyuddine@gmail.com>
2025-10-14 19:45:24 +0500

    Configuration +

---
+++ main/java/org/example/backend_14_1/Backend141Application.java
@@ -0,0 +1,15 @@
+package org.example.backend_14_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend141Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend141Application.class, args);
+    }
+
+
+
+}
---
+++ test/java/org/example/backend_14_1/Backend141ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_14_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend141ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
